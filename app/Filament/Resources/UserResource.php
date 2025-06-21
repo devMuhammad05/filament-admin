a87103cef5f1b21b3870ba6d4d1b3362c8e1bf3e
Muhammad Yahaya <adelekeyahaya05@gmail.com>
2025-06-22 00:34:58 +0100

    Add UserResource form fields and create Category and Post models with migrations

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,9 +6,12 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +20,19 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')->required(),
+                TextInput::make('email')->required(),
+                TextInput::make('password')->password()->visibleOn('edit'),
+                // Select::make('name')->options([
+                //     'test' => "test",
+                //     'name' => "Muhammad",
+                // ])
             ]);
     }
 
@@ -31,7 +40,8 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name'),
+                TextColumn::make('email'),
             ])
             ->filters([
                 //
